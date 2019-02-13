d232446924ec2eb9e144ba0eaddc69272fa210c4
Russell Moore <RussellM@batc.local>
2019-02-13 08:51:07 -0700

    first up load

---
+++ temp_resources/links_notes.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+// Google doc spread sheet
+// https://docs.google.com/spreadsheets/d/12eQoBNTZ0P7Z9JBxdDAnPp9wPEApiQr0qT8SZiaGgrw/edit?usp=sharing
+
+// Google drive folder
+// https://drive.google.com/drive/folders/1-M9EGD2yekMsMS8HziWm5NWIdt7rn17l?usp=sharing
---
+++ temp_resources/old_notes/_notes.js
@@ -0,0 +1,51 @@
+// @ site, admin site
+// pinterest, mine
+
+// @ site ideas
+// pinterest, mine
+// https://lavalite.org/
+// ---
+//. ??? look at https://www.primefaces.org/primeng/#/
+
+// @ over all goal
+// powerful, easy, pretty
+// customizable
+// fun, bold, but doesn't get in the way of working
+
+// @ like
+// Toggle
+//. Todoist
+// Adilas
+// MinsterTask
+// trello
+
+
+// @ react libraries
+// Material-ui
+// https://material-ui.com/
+// provides material components
+// or
+// Materialize
+// css and js???
+// https://materializecss.com/
+
+
+// @ resources
+// CSS masking, icon animations
+// https://www.youtube.com/watch?v=jSmp3osrQiA
+
+// css modules
+// Also have examples in project folders: cssmodules - look up useing scss
+// https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/12001122?start=0
+
+// css variables 
+// https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_variables
+// https://stackoverflow.com/questions/17893823/how-to-pass-parameters-to-css-classes
+// https://www.w3schools.com/css/css3_variables.asp
+
+// color pickers
+// https://casesandberg.github.io/react-color/
+
+// @ Possibly use resources
+// Return adjacent jsx Code
+// https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8672134?start=0
