a23e92ff738d3275619e8933a123164780e10e63
Valtross <peterson.glock17@gmail.com>
2019-03-19 19:07:15 -0600

    Finished the Tag class. Beginning work on the Label class.

--- private/classes/tag.class.php
+++ private/classes/tag.class.php
@@ -1,41 +1,81 @@
 <?php
-    class Tag {
-        // @ ----- START OF ACTIVE RECORD CODE -----
-            // database connection
-            static protected $database;
-            static protected $db_columns = ['id', 'note', 'title'];
-
-            // set up local reference for the database
-            static public function set_database($database) {
-                self::$database = $database;
-            }
-
+    class Tag extends DatabaseObject {
+        // @ class database information start
+            // Class specific properties. Overwritten from DabaseObject Class
+            // Name of the table
+            static protected $tablename = 'tags';
+            // db columns, if need to exclude particular column excluded in the database object attributes()
+            static protected $columns = ['id', 'note', 'title'];
+            // values to exclude on normal updates, should always include id
+            static protected $columnExclusions = ['id'];
+            // name specific properties you wish to included in the API
+            static protected $apiProperties = [];
+            // * collection_type_reference, located at: root/private/reference_information.php
+            static protected $collectionTypeReference = 0;
+            // db validation, // * validation_options located at: root/private/reference_information.php
             // get all post tags
-            static public function find_all_post_tags() {
-                $sql = "SELECT id, title FROM tags WHERE useTag = 1 ORDER BY title";
+            static protected $validation_columns = [
+                'id' => [
+                    'name' => 'Tag id',
+                    'required' => 'yes',
+                    'type' => 'int',
+                    'num_min' => 0,
+                    'max' => 10
+                ],
+                'note' => [
+                    'name' => 'Tag Note',
+                    'required' => 'no',
+                    'type' => 'str',
+                    'max' => 10
+                ],
+                'title' => [
+                    'name' => 'Tag Title',
+                    'required' => 'yes',
+                    'type' => 'str',
+                    'max' => 50
+                ]
+            ];
+
+        // @ class database information end
+
+        // @ class specific queries start
+            // Find all the tags associated with the collection type parameter // * collection_type_reference, located at: root/private/reference_information.php
+            static public function find_all_tags(int $type = 0) {
+                $sql = "SELECT id, note, title, useTag FROM tags ";
+                if (!($type > 4 && $type < 0)) {
+                    $sql .= "WHERE useTag = {$type}";
+                }  
                 return self::find_by_sql($sql);
             }
+        // @ class specific queries end
 
-            // find by sql
-            static public function find_by_sql($sql) {
-                $result = self::$database->query($sql);
-                // error handling
-                if (!$result) {
-                    exit("Query Failed!!!: " . self::$database->error);
-                } 
-                // turn results into an array of objects
-                $object_array = [];
-                // loop through query
-                while ($record = $result->fetch_assoc()) {
-                    $object_array[] = self::instantiate($record);    
-                }
-                //free up query result
-                $result->free();
-                // return an array of populated objects
-                return $object_array;   
+        // @ properties start
+            // main properties
+            public $title;
+            public $note;
+            public $useTag;
+
+            //protected properties
+            protected $id;
+
+        // @ properties end
+
+        // @ methods start
+            // constructor method, type declaration of array
+            public function __construct(array $args=[]) {
+                $this->id = $args['id'] ?? NULL;
+                $this->note = $args['note'] ?? NULL;
+                $this->title = $args['title'] ?? NULL;
+                $this->useTag = $args['useTag'] ?? NULL;
             }
-        // @ ----- END OF ACTIVE RECORD CODE -----
-        
+
+            // methods
+            // get the id
+            public function get_id() {
+                return $this->id;
+            }
+
+        // @ methods end
     }
     
 ?>
