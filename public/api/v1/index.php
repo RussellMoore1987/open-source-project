2667d76c39f9a2050a65791e8e2a9af9a9f9cb0a
Valtross <peterson.glock17@gmail.com>
2019-03-27 18:32:42 -0600

    Restructured the code to make it more understandable. Still working on json packaging.

--- public/api/v1/index.php
+++ public/api/v1/index.php
@@ -10,6 +10,8 @@
 // 2- Dynamic termsOfUse
 // 3- Dynamic contact
 // 4- Dynamic root
+// 5- Force HTTPS communication
+// 6- HTTP status codes and error returning
 
 // 1st Level - Genral Info
 $generalInfo = array(
@@ -24,49 +26,59 @@ $generalInfo = array(
 );
 
 // 2nd Level - mainAuthentication and route endpoints
-$authAndRoutes = array(
+$routes = array(
     "mainAuthentication" => NULL,
     "/categories" => NULL
 );
 
 // 3rd Level - category methods and mainAuthentication data
-$mainAuthData = array(
+$mainAuthentication = array(
     "required" => "If the system has an API key, it is required on all requests",
     "default" => "none",
     "example" => "root/public/api/v1/categories/?authToken=12466486351864sd4f8164g89rt6rgfsdfunwiuf74"
 );
 
-$categoryMethods = array(
+$categories = array(
     "methods" => NULL
 );
 
 // 4th Level - available methods, GET, POST
-$availableMethods = array(
-    "availableMethods" => NULL
-);
-
-$getMethod = array(
-    "GET" => NULL
-);
-
-$postMethod = array(
+$methods = array(
+    "availableMethods" => NULL,
+    "GET" => NULL,
     "POST" => NULL
 );
 
-// Package all our arrays for the 4th level
-$fourthLevelPackage[] = $availableMethods;
-$fourthLevelPackage[] = $getMethod;
-$fourthLevelPackage[] = $postMethod;
+// 5th level - available GET, POST, parameters
+$availableMethods = array(
+    "GET" => "to get information",
+    "POST" => "to post information"
+);
+
+$get = array(
+    "parameters" => NULL
+);
+
+// TODO: put POST information here
+
+// 6th level - no params, id, createdDate, greaterThan, lessThan, ctr
+
 
 // You must link your json in reverse from the bottom of the structure to the top for it to package correctly
 
+// Link the 4th and 5th Levels
+$methods['availableMethods'] = $availableMethods;
+$methods['GET'] = $get;
+
 // Link the 3rd and 4th levels
-$categoriesMethods['methods'] = $fourthLevelPackage;
+$categories['methods'] = $methods;
+
 // Link the 2nd and 3rd Levels
-$authAndRoutes['mainAuthentication'] = $mainAuthData;
-$authAndRoutes['/categories'] = $categoryMethods;
+$routes['mainAuthentication'] = $mainAuthentication;
+$routes['/categories'] = $categories;
+
 // Link the 1st and 2nd Levels
-$generalInfo['routes'] = $authAndRoutes;
+$generalInfo['routes'] = $routes;
 
 $jsonData = json_encode($generalInfo);
 
