a3e52341aeeea3e3e48dd836c7df98af3c08f970
Valtross <peterson.glock17@gmail.com>
2019-03-27 18:19:09 -0600

    Begininng of the json array complete

--- public/api/v1/index.php
+++ public/api/v1/index.php
@@ -11,7 +11,7 @@
 // 3- Dynamic contact
 // 4- Dynamic root
 
-// package our top level info - TOP Level
+// 1st Level - Genral Info
 $generalInfo = array(
     "companyName" => "Placeholder Company",
     "termsOfUse" => "Placeholder Terms URL",
@@ -19,18 +19,54 @@ $generalInfo = array(
     "contact" => "someone@someone.com",
     "description" => "Placeholder Description",
     "root" => "https://www.placeholdersite.com",
+    "mainPath" => "/public/api/v1/",
     "routes" => NULL
 );
 
-// package our main authentication - 2nd Level
-$mainAuthentication = array(
+// 2nd Level - mainAuthentication and route endpoints
+$authAndRoutes = array(
+    "mainAuthentication" => NULL,
+    "/categories" => NULL
+);
+
+// 3rd Level - category methods and mainAuthentication data
+$mainAuthData = array(
     "required" => "If the system has an API key, it is required on all requests",
     "default" => "none",
     "example" => "root/public/api/v1/categories/?authToken=12466486351864sd4f8164g89rt6rgfsdfunwiuf74"
 );
 
-// package all of our array together and send it back
-$generalInfo['routes'] = $mainAuthentication;
+$categoryMethods = array(
+    "methods" => NULL
+);
+
+// 4th Level - available methods, GET, POST
+$availableMethods = array(
+    "availableMethods" => NULL
+);
+
+$getMethod = array(
+    "GET" => NULL
+);
+
+$postMethod = array(
+    "POST" => NULL
+);
+
+// Package all our arrays for the 4th level
+$fourthLevelPackage[] = $availableMethods;
+$fourthLevelPackage[] = $getMethod;
+$fourthLevelPackage[] = $postMethod;
+
+// You must link your json in reverse from the bottom of the structure to the top for it to package correctly
+
+// Link the 3rd and 4th levels
+$categoriesMethods['methods'] = $fourthLevelPackage;
+// Link the 2nd and 3rd Levels
+$authAndRoutes['mainAuthentication'] = $mainAuthData;
+$authAndRoutes['/categories'] = $categoryMethods;
+// Link the 1st and 2nd Levels
+$generalInfo['routes'] = $authAndRoutes;
 
 $jsonData = json_encode($generalInfo);
 
