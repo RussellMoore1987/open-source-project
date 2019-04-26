24210697c1bc99c74ff50130e46aa65c51a9e1cd
Russell Moore <RussellM@batc.local>
2019-04-26 17:34:10 -0600

    made some html changes

--- temp_html/general.js
+++ temp_html/general.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 
     // # header
         // header drop-down menu functionality
-        $("header .dropdown").on( "click", function() {
+        $(".header_dropdown").on( "click", function() {
             // get menu
-            const dropdown_menu = $("header .dropdown_menu");
+            const dropdown_menu = $(".header_dropdown_menu");
             // performed information accordingly
             if (dropdown_menu.hasClass("menu_down")) {
                 // toggle class name
@@ -31,7 +31,7 @@ $(document).ready(function() {
             }
         });
         // book mark functionality
-        $("header .header_bookmark").on( "click", function() {
+        $(".header_bookmark").on( "click", function() {
             // check to see what class is currently there
             if ($(this).hasClass('far')) {
                 // set bookmark
@@ -43,4 +43,50 @@ $(document).ready(function() {
                 $(this).addClass('far');
             }
         });
+    // # side bar
+        // menu open and close
+        $(".dst i").on( "click", function() {
+            $(this).toggleClass('dst_i_active');
+            $('.ds').toggleClass('ds_open');
+        });
+        // expand interface/dashboard
+        $(".ds_btn_expand").on( "click", function(e) {
+            // prevent default behavior
+            e.preventDefault();
+            // reset icon on button
+            const icon = $(this).find("i");
+            // check to see what icon to set
+            if (icon.hasClass("fa-expand-arrows-alt")) {
+                icon.removeClass("fa-expand-arrows-alt")  
+                icon.addClass("fa-compress-arrows-alt")  
+            } else {
+                icon.removeClass("fa-compress-arrows-alt")
+                icon.addClass("fa-expand-arrows-alt")  
+            }
+            $(".d_container").toggleClass('d_box');
+        });
+    // # right side bar
+        // open right side bar
+        $(".header_default_icons .fa-check-circle").on( "click", function() {
+            $('.dso').toggleClass('dso_open');
+            $('.dmf').toggleClass('dmf_open');
+            $('.dso_modal').toggleClass('dso_modal_open');
+        });
+        // open ability to close right side bar 
+        $(".dso_modal").on( "click", function() {
+            $('.dso').toggleClass('dso_open');
+            $('.dmf').toggleClass('dmf_open');
+            $('.dso_modal').toggleClass('dso_modal_open');
+        });
+    // # media content
+        // open media content modal // ! not all it needs to be
+        $(".header_default_icons .fa-search").on( "click", function() {
+            $('.dm').toggleClass('dm_open');
+            $('.dmc_modal').toggleClass('dmc_modal_open');
+        });
+        $(".dmc_modal").on( "click", function() {
+            $('.dm').toggleClass('dm_open');
+            $('.dmc_modal').toggleClass('dmc_modal_open');
+        });
+    
 });
--- temp_html/index.php
+++ temp_html/index.php
@@ -7,37 +7,42 @@
     <title>Document</title>
     <link rel="icon" type="image/png" href="images/logo-02.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700i" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
-<!-- pattern_bg full_bg -->
+<!-- body bg options are, pattern_bg full_bg -->
 <body class="full_bg">
-    <div class="dashboard_container box">
+    <!-- dashboard end -->
+    <div class="d_container d_box">
+        <!-- dashboard header -->
         <header>
             <div>
                 <i class="far fa-bookmark header_bookmark"></i>
-                <a href=""><img class="logo" src="images/logo.png" alt=""></a>
+                <a href=""><img class="header_logo" src="images/logo.png" alt=""></a>
             </div>
             <div>
                 <div class="header_default_icons">
                     <i class="fas fa-search"></i>
                     <i class="far fa-check-circle"></i>
                 </div>
-                <div class="dropdown">
+                <div class="header_dropdown">
                     <div>
                         <h4>Russell Moore</h4>
-                        <span>Developer</span>
-                        <div class="dropdown_menu">
+                        <span class="header_job_title">Developer</span>
+                        <div class="header_dropdown_menu">
                             <div>
                                 <a href=""><i class="fas fa-user"></i> My Profile</a>
-                                <a href="" class="my_posts"><i class="fas fa-scroll"></i> My Posts</a>
+                                <a href=""><i class="fas fa-file-alt"></i> My Posts</a>
                                 <a href=""><i class="fas fa-file-invoice"></i> My Contents</a>
                             </div>
                             <hr>
                             <div>
                                 <h5>Book Marks</h5>
-                                <a href=""><i class="fas fa-bookmark"></i> add_edit_post 2/3/19 <i class="fas fa-times"></i></a>
-                                <a href=""><i class="fas fa-bookmark"></i> add_edit_post 2/3/19 <i class="fas fa-times"></i></a>
-                                <a href=""><i class="fas fa-bookmark"></i> add_edit_post 2/3/19 <i class="fas fa-times"></i></a>
+                                <div class="header_bookmark_list">
+                                    <a href=""><i class="fas fa-bookmark"></i> add_edit_post 2/3/19</a><i class="fas fa-times"></i>
+                                    <a href=""><i class="fas fa-bookmark"></i> add_edit_post 2/3/19</a><i class="fas fa-times"></i>
+                                    <a href=""><i class="fas fa-bookmark"></i> add_edit_post 2/3/19</a><i class="fas fa-times"></i>
+                                </div>
                             </div>
                             <hr>
                             <div>
@@ -45,20 +50,90 @@
                             </div>
                         </div>
                     </div>
-                    <img class="profile" src="images/profile.png" alt="">
+                    <img class="header_profile" src="images/profile.png" alt="">
                     <small><i class="fas fa-chevron-down"></i></small>
                 </div>
             </div>
         </header>
-        <div class="dashboard_sidebar">
-            <div class="dashboard_sidebar_top"></div>
-            <div class="dashboard_sidebar_middle"></div>
-            <div class="dashboard_sidebar_bottom"></div>
+        <!-- dashboard header end -->
+
+        <!-- dashboard sidebar -->
+        <div class="ds">
+            <!-- sidebar top -->
+            <div class="dst">
+                <i class="fas fa-align-left"></i>
+            </div>
+            <!-- sidebar top end -->
+
+            <!-- sidebar middle, class="active" -->
+            <div class="dsm">
+                <!-- //todo: not what final structure will look like -->
+                <a href=""><i class="fas fa-th-large"></i><span> Main Dashboard</span></a>
+                <a href="" class="active"><i class="fas fa-file-invoice"></i><span> Content</span></a>
+                <a href=""><i class="fas fa-user-friends"></i><span> Users</span></a>
+                <a href=""><i class="fas fa-file-alt"></i><span> Posts</span></a>
+                <a href=""><i class="fas fa-folder"></i><span> Categories</span></a>
+                <a href=""><i class="fas fa-tags"></i><span> Tags</span></a>
+                <a href=""><i class="fas fa-list-alt"></i><span> Labels</span></a>
+                <a href=""><i class="fas fa-comment-dots"></i><span> Comments</span></a>
+                <a href=""><i class="fas fa-bookmark"></i><span> BookMarks</span></a>
+                <a href=""><i class="fas fa-search"></i><span> Search</span></a>
+                <!-- //todo: finish drop-down menus at some point -->
+                <!-- <div class="dsm_dropdown">
+                    <a href="" class="active"><i class="fas fa-file-invoice"></i><span> Content</span></a>
+                    <div class="dsm_dropdown_menu">
+                        <a href=""><i class="fas fa-file-invoice"></i><span> Main Dashboard</span></a>
+                        <a href=""><i class="fas fa-file-invoice"></i><span> Main Dashboard</span></a>
+                    </div>
+                </div> -->
+            </div>
+            <!-- sidebar middle end -->
+
+            <!-- sidebar bottom -->
+            <div class="dsb">
+                <a href="" class="btn_box"><i class="fas fa-lightbulb"></i></a>
+                <a href="" class="btn_box ds_btn_expand"><i class="fas fa-expand-arrows-alt"></i></a>
+            </div>
+            <!-- sidebar bottom end -->
         </div>
-        <!-- right now just the to do list -->
-        <div class="dashboard_sidebar_options"></div>
-        <div class="dashboard_body"></div>
+        <!-- dashboard sidebar end -->
+
+        <!-- dashboard body-->
+        <div class="db">
+        </div>
+        <!-- dashboard body end -->
+
+        <!-- dashboard modals -->
+        <div class="dm">
+            <div class="dmc_modal"></div>    
+        </div>
+
+        <!-- dashboard modals full dashboard layout -->
+        <div class="dmf">
+            <div class="dso_modal"></div>    
+        </div>
+
+        <!-- dashboard right sidebar options, right now just the to do list -->
+        <div class="dso">
+            <div>
+                <div>
+                    <i class="far fa-check-circle"></i>
+                </div>
+                <hr>
+                <div>
+                    <div></div>
+                </div>
+                <div class="dso_bottom">
+                    <form>
+                        <input type="text">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- dashboard right sidebar options end -->
+
     </div>
+    <!-- dashboard end -->
     
     <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
