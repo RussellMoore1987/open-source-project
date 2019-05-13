9860a45fab0b415c3af3b7714ea3834c918167bf
Valtross <peterson.glock17@gmail.com>
2019-05-13 17:47:28 -0600

    Creating the code for the first layout.

--- private/layouts/devToolKit_createDropTable.layout.php
+++ private/layouts/devToolKit_createDropTable.layout.php
@@ -1,6 +1,26 @@
 <?php
 
-// TODO: Add code for createDropTable layout
-// TODO: Come up with a better name for the layout
+// TODO: Logic for accepting GET / POST requests
+// Check if it is a get request
+if(is_get_request()) {
+    
+}
+// TODO: Logic for creating, dropping, and inserting Faker Data
 
 ?>
+
+<h2>[ClassName]</h2>
+<h2>[Response / Error Messages]</h2>
+
+<!-- TODO: Where to submit the form data? -->
+<!-- Create Table -->
+<button type="submit">Create Table</button>
+
+<!-- Drop Table -->
+<button type="submit">Drop Table</button>
+
+<!-- Insert Faker Data -->
+<form id="faker_form">
+<input type="number" placeholder="# of Records">
+<button type="submit" form="faker_form">Insert</button>
+</form>
